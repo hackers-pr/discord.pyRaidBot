e6930537aaf06fcedf21c64b112a4ed02152c84e
hackers-pr <58606521+hackers-pr@users.noreply.github.com>
2020-11-15 16:38:28 +0300

    v0.2

--- builder.py
+++ builder.py
@@ -1,9 +1,11 @@
 import os
+import os.path
+os.system('cd temp')
 
-def rbst(random):
+def rbst(token_filename, random):
 
-  if random!='ry'and random!='rn':
-    random='rn'
+  if random!='random'and random!='normal':
+    random='normal'
     
   global number_of_bots
 
@@ -16,12 +18,8 @@ def rbst(random):
   nbots_help=-1
   if config_file.read()=='':
     number_of_bots=int(input('With how many bots you will spam?\n'))
-    verify=input('Does the required server have a verification system? (y\n)\n')
-    verify_message=input('Now pick a message, what bots will send TO VERIFY.\n')
     message_to_send=input('Now pick a message, what bots will send.\n')
-    verify_channel=input('Input verification channel ID.\n')
     message_channel=input('Input needed channel ID (where you will spam).\n')
-    token_filename=input('Name a file in rbot folder with tokens\n')
   
   else:
     config_file=open('cnfg.log', 'r')
@@ -46,12 +44,9 @@ def rbst(random):
     temppy.write(temppy.read()+'@client.event\n')
     temppy.write(temppy.read()+'async def on_connect():\n')
     temppy.write(temppy.read()+'  text=""\n')
-    if verify=='y':
-      temppy.write(temppy.read()+'  ch=client.get_channel('+verify_channel+')\n')
-      temppy.write(temppy.read()+'  await ch.send("'+verify_message+'")\n')
     temppy.write(temppy.read()+'  ch=client.get_channel('+message_channel+')\n')
     temppy.write(temppy.read()+'  while True:\n')
-    if random=='ry':
+    if random=='random':
       temppy.write(temppy.read()+'      for x in range(1999):\n')
       temppy.write(temppy.read()+'        text+=random.choice(string.ascii_letters)\n')
       temppy.write(temppy.read()+'      await ch.send(text)\n')
@@ -68,6 +63,43 @@ def rbst(random):
   for x in range(number_of_bots):
     nbots_help+=1
     os.system('start /min temppy'+str(nbots_help)+'.py')
+
+def verify(token_filename, versystem):
+
+  nbots_help=-1
+
+  os.system('del verpy*.py')
+  os.system('cls')
+  
+  verify_channel=input('Input verification channel ID.\n')
+  if versystem=='message':
+    verify_message=input('Now pick a message, what bots will send to verify.\n')
+  number_of_bots=int(input('How many bots you need to verify?\n'))
+    
+  token_file=open(token_filename, 'r')
+  token=token_file.read().split(' ')
+  token_file.close()
+  
+  for x in range(number_of_bots):
+  
+    nbots_help+=1
+    verpy=open('verpy'+str(nbots_help)+'.py', 'a+')   
+    verpy.write('import discord; client=discord.Client()\n')
+    verpy.write(verpy.read()+'@client.event\n')
+    verpy.write(verpy.read()+'async def on_connect():\n')
+    verpy.write(verpy.read()+'  ch=client.get_channel('+verify_channel+')\n')
+    verpy.write(verpy.read()+'  await ch.send("'+verify_message+'")\n')
+    verpy.write(verpy.read()+'  exit()\n')
+    verpy.write(verpy.read()+'client.run("'+token[nbots_help]+'", bot=False)\n')
+    verpy.close()
+    print('The settings of the №'+str(nbots_help+1)+' bot were written')
+
+  nbots_help=-1
+  print('Settings done, starting bot...')  
+    
+  for x in range(number_of_bots):
+    nbots_help+=1
+    os.system('start /min verpy'+str(nbots_help)+'.py')
   
 def rbtk():
   try:
@@ -80,7 +112,8 @@ def rbtk():
     os.system('taskkill /f /im temppy'+str(nbots_help)+'.py')
 
 def rbcnfg():
-
+  os.system('cd ..')
+  
   global number_of_bots
 
   config_file=open('cnfg.log', 'w')
@@ -90,10 +123,15 @@ def rbcnfg():
   message_to_send=input('Now pick a message, what bots will send.\n')
   verify_channel=input('Input verification channel ID.\n')
   message_channel=input('Input needed channel ID (where you will spam).\n')
-  token_filename=input('Name a file in rbot folder with bots token\n')
+  token_filename=input('Name a file in rbot folder with bots token\n') 
   config_file.write(str(number_of_bots)+' '+verify+' '+verify_message+' '+message_to_send+' '+verify_channel+' '+message_channel+' '+token_filename)
   
+  os.system('cd temp')
+  
 def unchng(eoaou, sthing_to_chng, password):
+  os.system('del usch*.py')
+  os.system('cls')
+
   if eoaou!='email' and eoaou!='avatar' and eoaou!='username':
     print('error: please input, what you want to change')
     return
@@ -133,55 +171,23 @@ def unchng(eoaou, sthing_to_chng, password):
   for x in range(number_of_bots):
     nbots_help+=1
     os.system('start /min usch'+str(nbots_help)+'.py')
-    
-def uinfo(member_id, token):
-  os.system('del uinfoh.py')
-  os.system('cls')
-  print('Building uinfoh.py...')
-  uinfoh=open('uinfoh.py', 'a+')
-  uinfoh.write(uinfoh.read()+'import discord \n')
-  uinfoh.write(uinfoh.read()+'from tkinter import *\n')
-  uinfoh.write(uinfoh.read()+'import tkinter.messagebox \n')
-  uinfoh.write(uinfoh.read()+'client=discord.Client()\n')
-  uinfoh.write(uinfoh.read()+'@client.event\n')
-  uinfoh.write(uinfoh.read()+'async def on_connect():\n')
-  uinfoh.write(uinfoh.read()+'  user=client.get_user('+member_id+')\n')
-  uinfoh.write(uinfoh.read()+'  tkinter.messagebox.showinfo("About "+user.name+":", \n')
-  uinfoh.write(uinfoh.read()+'    "\\'+'nID: "+str(user.id)+\n')
-  uinfoh.write(uinfoh.read()+'    "\\'+'ndiscriminator: "+user.discriminator+\n')
-  uinfoh.write(uinfoh.read()+'    "\\'+'nbot: "+str(user.bot)+\n')
-  uinfoh.write(uinfoh.read()+'    "\\'+'nsystem: "+str(user.system)+\n')
-  uinfoh.write(uinfoh.read()+'    "\\'+'nrelationship: "+str(user.relationship)+\n')
-  uinfoh.write(uinfoh.read()+'    "\\'+'ncreated at: "+str(user.created_at)+\n')
-  uinfoh.write(uinfoh.read()+'    "\\'+'navatar url: "+str(user.avatar_url)+\n')
-  uinfoh.write(uinfoh.read()+'    "\\'+'nis avatar animated: "+str(user.is_avatar_animated())+\n')
-  uinfoh.write(uinfoh.read()+'    "\\'+'nmention: "+str(user.mention)+\n')
-  uinfoh.write(uinfoh.read()+'    "\\'+'nuser color: "+str(user.color)+\n')
-  uinfoh.write(uinfoh.read()+'    "\\'+'nuser is your friend: "+str(user.is_friend())+\n')
-  uinfoh.write(uinfoh.read()+'    "\\'+'nuser is blocked: "+str(user.is_blocked()))\n')
-  uinfoh.write(uinfoh.read()+'    exit()\n')
-  uinfoh.write(uinfoh.read()+'  client.run("'+token+'", bot=False)')
-  uinfoh.close()
-  print('Running uinfoh.py...')
-  os.system('start /min uinfoh.py')
   
 def dthelp():
   print('\nSELFBOT COMMANDS:'+
-  '\n\nrbst [rn/ry]: start raidbot;'+
+  '\n\nrbst [file with tokens name] [random/normal]: start raidbot;'+
   '\nrbtk: taskkill raidbot;'+
   '\nrbcnfg: set rbst config;'+
   '\nunchng [what you need to change, email/avatar/username, password]: change something;'+
-  '\nuinfo [user id, token]: shows user info. may not work'
   '\n\nOTHER:'+
   '\n\nhelp: shows this message;'+
   '\nexit: exit.')
   print('\n\n#######################\n'+
   '\nCOMMAND EXAMPLES:\n'+
-  '\nrbst ry'+
+  '\nrbst bots.txt random'+
+  '\nverify tokens.log message'+
   '\nhelp'+
   '\nunchng email very_cool@email.com good_password9281'+
   '\nunchng avatar animeavatar.png 02011998'+
-  '\nuinfo 527531639019012150 NzNuDI-lEtotally-fake-tokenOd-Ew'+
   '\n\nMore examples will come with more commands!')
 
 print("print help if you don't know what to do ― hackers_pr")
@@ -198,11 +204,17 @@ while 1:
   elif inpc=='exit':
       exit()    
   elif inpcl[0]=='rbst':
-    if len(inpcl)>1:
-      rbst(inpcl[1])
+    if len(inpcl)>2:
+      rbst(inpcl[1], inpcl[2])
+    else:
+      print('not enough args')
   elif inpcl[0]=='unchng':
     if len(inpcl)>3:      
       unchng(eoaou=inpcl[1], sthing_to_chng=inpcl[2], password=inpcl[3])
-  elif inpcl[0]=='uinfo':
+    else:
+      print('not enough args')
+  elif inpcl[0]=='verify':
     if len(inpcl)>2:
-      uinfo(inpcl[1], inpcl[2])
+      verify(inpcl[1], inpcl[2])
+  else:
+    print('unknown command')
